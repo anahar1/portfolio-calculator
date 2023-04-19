36cf06cc8feeab87e8cd7d7c6d923c1c6da35587
nossherlock <anahar1@asu.edu>
2023-04-19 00:26:50 -0700

    Skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { useState } from 'react';
 import { addUser } from './firebase.js';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Tasks from './Tasks.js';
 
 const App = () => {
   const [name, setName] = useState('');
+  const [navigation, setNavigation] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await addUser(name);
-    setName('');
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    addUser(name).then(() => {
+      setName('');
+      setNavigation(true);
+    }).catch((error) => {
+      console.log('Error adding name to Firestore', error);
+    });
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <button type="submit">Add Name</button>
-      </form>
-    </div>
+    <>
+      {navigation ? (
+        <Navigate to="/tasks" />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <button type="submit">Add Name</button>
+        </form>
+      )}
+      <Routes>
+        <Route path='/tasks' element={<Tasks />} />
+      </Routes>
+    </>
   );
 };
 
